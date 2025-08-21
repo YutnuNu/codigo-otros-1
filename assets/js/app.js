fde246a637a1add8e6de1d77342892d556b83e74
Yutnu <yutnuhernandez@gmail.com>
2025-08-21 11:53:29 -0600

    Se corrigen errores y hacen anotaciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,23 @@
+//obtiene datos de una API
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
+//al ser un querrySelector debe de llevar un .name/blog al ser una clase
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
 const $l = document.querySelector('.location');
-
-function displayUser(username) {
+//al usar await debe ser una funcion asincrona (async)
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //definimos data, debera obtener lo que venga de response por lo
+  //tanto agregamos el await e insertarlo en el html
+  const data =  await response.json();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  //como queremos insertar lo que viene de la API quitamos las comillas y ${}
+  //ya que son datos
+  $n.textContent = data.name;
+  $b.textContent = data.blog;
+  $l.textContent = data.location;
 }
 
 function handleError(err) {
